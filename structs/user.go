845985ddaa16db82c0a62ecb134d950b7d810a16
Mahmoud AlAsad Workstation <mahmoud.alasad@csuglobal.edu>
2022-01-25 12:17:47 +0400

    Removed
    Removed user profile from DB and no longer required to add from google as data

--- structs/user.go
+++ structs/user.go
@@ -1,12 +1,7 @@
 package structs
 
 type UserLogin struct {
-	Userid              uint   `gorm:"column:userID; PRIMARY_KEY" json:"userID"`
-	ClaimUserSub        string `json:"sub"`
-	ClaimUserEmail      string `json:"email"`
-	ClaimUserFaimlyname string `json:"family_name"`
-	ClaimUserGivenname  string `json:"given_name"`
-	ClaimUserLocale     string `json:"locale"`
-	ClaimUserName       string `json:"name"`
-	ClaimUserPicture    string `json:"picture"`
+	Userid         uint   `gorm:"column:userID; PRIMARY_KEY" json:"userID"`
+	ClaimUserSub   string `json:"sub"`
+	ClaimUserEmail string `json:"email"`
 }
